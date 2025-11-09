12a35ec3c7f9403bab1edb90c0f21591c41ec6cb
[dyad] <git@dyad.sh>
2025-11-09 14:02:39 +0530

    [dyad] Reverting PasswordInputWithToggle and suggesting a full rebuild to fix persistent syntax error. - wrote 1 file(s)

--- src/components/PasswordInputWithToggle.tsx
+++ src/components/PasswordInputWithToggle.tsx
@@ -1,39 +1,20 @@
 "use client";
 
 import * as React from "react";
-import { Eye, EyeOff } from "lucide-react";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 interface PasswordInputWithToggleProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const PasswordInputWithToggle = React.forwardRef<HTMLInputElement, PasswordInputWithToggleProps>(
-  ({ className, type, ...props }, ref) => {
-    const [showPassword, setShowPassword] = React.useState(false);
-
+  ({ className, ...props }, ref) => {
     return (
-      <div className="relative">
+      <div className={cn("relative", className)}>
         <Input
-          type={showPassword ? "text" : "password"}
-          className={cn("pr-10", className)}
+          type="password"
           ref={ref}
           {...props}
         />
-        <Button
-          type="button"
-          // Temporarily making the button very visible
-          className="absolute right-0 top-0 h-full px-3 py-2 bg-blue-500 hover:bg-blue-600 text-white z-10"
-          onClick={() => setShowPassword((prev) => !prev)}
-          disabled={props.disabled}
-        >
-          {showPassword ? (
-            <EyeOff className="h-4 w-4 text-white" /> {/* Icon color changed to white */}
-          ) : (
-            <Eye className="h-4 w-4 text-white" /> {/* Icon color changed to white */}
-          )}
-          <span className="sr-only">{showPassword ? "Hide password" : "Show password"}</span>
-        </Button>
       </div>
     );
   }
